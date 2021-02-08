3dfb45a3cdc5e65094420d2eba2a76e7d31a621d
azamatfarkhodov <farkhodovazamat@gmail.com>
2021-02-08 19:33:02 +0500

    first-commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+          crossorigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
+          integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
+          crossorigin="anonymous"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,401 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import {
+  Container,
+  Row,
+  Col,
+  CardDeck,
+  Card,
+  Image,
+  Jumbotron,
+} from "react-bootstrap";
+import axios from "axios";
+import Pagination from "react-js-pagination";
 
-export default function Home() {
+export default function Home({ bedrooms, kitchens, childrooms, offices }) {
+  const productsPerPage = 4;
+  const [activePage, setCurrentPage] = useState(1);
+
+  const indexOfLastProduct = activePage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Wooden Каталог</title>
       </Head>
+      <header>
+        <nav className={styles.nav}>
+          <Image
+            src="/favicon.ico"
+            alt="Picture of the company"
+            width={50}
+            height={50}
+          />
+          <h1>WOODEN</h1>
+        </nav>
+      </header>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Container className="py-5">
+        <Row className={styles.row}>
+          <Col sm={6} className={styles.hero_left}>
+            <Image src="/wooden.jpg" alt="Company logo" width="100%" />
+          </Col>
+          <Col sm={6} className={styles.hero_right}>
+            <h1>Каталог Комапании Wooden</h1>
+            <div className={styles.blockquote}>
+              <h1>Семья — это фирма, а фирма — это семья</h1>
+              <h4>&mdash; Слоган компании IKEA</h4>
+            </div>
+          </Col>
+        </Row>
+      </Container>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <Container className="mt-5">
+        <Row>
+          <Col className={styles.title}>
+            <h1>Для Спальни</h1>
+            <CardDeck>
+              {bedrooms
+                .slice(indexOfFirstProduct, indexOfLastProduct)
+                .map((product) => (
+                  <Col
+                    sm={12}
+                    md={6}
+                    lg={4}
+                    xl={3}
+                    key={product.id}
+                    className={styles.carddeck}
+                  >
+                    <Card style={{ width: "16rem" }}>
+                      <Card.Img
+                        variant="top"
+                        height="275px"
+                        style={{ objectFit: "cover" }}
+                        src={
+                          product.image
+                            ? product.image.url
+                            : "https://via.placeholder.com/150.png"
+                        }
+                      />
+                      <Card.Body className={styles.cardbody}>
+                        <Card.Title>Подробности в нашем</Card.Title>
+                        <Link href="https://t.me/wooden_mebellari">
+                          <a target="_blank">
+                            <Image
+                              src="/telegram.png"
+                              alt="Telegram logo"
+                              width={50}
+                              height={50}
+                            />
+                          </a>
+                        </Link>
+                        <Link href="https://instagram.com/wooden_jizzakh">
+                          <a target="_blank">
+                            <Image
+                              src="/instagram.png"
+                              alt="Telegram logo"
+                              width={50}
+                              height={50}
+                            />
+                          </a>
+                        </Link>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+            </CardDeck>
+            <div className={styles.pagination}>
+              <Pagination
+                itemClass="page-item"
+                linkClass="page-link"
+                activePage={activePage}
+                itemsCountPerPage={4}
+                totalItemsCount={bedrooms.length}
+                pageRangeDisplayed={5}
+                onChange={handlePageChange}
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      <Container>
+        <Row>
+          <Col className={styles.title}>
+            <h1>Для Кухни</h1>
+            <CardDeck>
+              {kitchens
+                .slice(indexOfFirstProduct, indexOfLastProduct)
+                .map((product) => (
+                  <Col
+                    sm={12}
+                    md={6}
+                    lg={4}
+                    xl={3}
+                    key={product.id}
+                    className={styles.carddeck}
+                  >
+                    <Card style={{ width: "16rem" }}>
+                      <Card.Img
+                        variant="top"
+                        height="275px"
+                        style={{ objectFit: "cover" }}
+                        src={
+                          product.image
+                            ? product.image.url
+                            : "https://via.placeholder.com/150.png"
+                        }
+                      />
+                      <Card.Body className={styles.cardbody}>
+                        <Card.Title>Подробности в нашем</Card.Title>
+                        <Link href="https://t.me/wooden_mebellari">
+                          <a target="_blank">
+                            <Image
+                              src="/telegram.png"
+                              alt="Telegram logo"
+                              width={50}
+                              height={50}
+                            />
+                          </a>
+                        </Link>
+                        <Link href="https://instagram.com/wooden_jizzakh">
+                          <a target="_blank">
+                            <Image
+                              src="/instagram.png"
+                              alt="Telegram logo"
+                              width={50}
+                              height={50}
+                            />
+                          </a>
+                        </Link>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+            </CardDeck>
+            <div className={styles.pagination}>
+              <Pagination
+                itemClass="page-item"
+                linkClass="page-link"
+                activePage={activePage}
+                itemsCountPerPage={4}
+                totalItemsCount={kitchens.length}
+                pageRangeDisplayed={5}
+                onChange={handlePageChange}
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+      <Container>
+        <Row>
+          <Col className={styles.title}>
+            <h1>Для Детской</h1>
+            <CardDeck>
+              {childrooms
+                .slice(indexOfFirstProduct, indexOfLastProduct)
+                .map((product) => (
+                  <Col
+                    sm={12}
+                    md={6}
+                    lg={4}
+                    xl={3}
+                    key={product.id}
+                    className={styles.carddeck}
+                  >
+                    <Card style={{ width: "16rem" }}>
+                      <Card.Img
+                        variant="top"
+                        height="275px"
+                        style={{ objectFit: "cover" }}
+                        src={
+                          product.image
+                            ? product.image.url
+                            : "https://via.placeholder.com/150.png"
+                        }
+                      />
+                      <Card.Body className={styles.cardbody}>
+                        <Card.Title>Подробности в нашем</Card.Title>
+                        <Link href="https://t.me/wooden_mebellari">
+                          <a target="_blank">
+                            <Image
+                              src="/telegram.png"
+                              alt="Telegram logo"
+                              width={50}
+                              height={50}
+                            />
+                          </a>
+                        </Link>
+                        <Link href="https://instagram.com/wooden_jizzakh">
+                          <a target="_blank">
+                            <Image
+                              src="/instagram.png"
+                              alt="Telegram logo"
+                              width={50}
+                              height={50}
+                            />
+                          </a>
+                        </Link>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+            </CardDeck>
+            <div className={styles.pagination}>
+              <Pagination
+                itemClass="page-item"
+                linkClass="page-link"
+                activePage={activePage}
+                itemsCountPerPage={4}
+                totalItemsCount={childrooms.length}
+                pageRangeDisplayed={5}
+                onChange={handlePageChange}
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+      <Container className="mb-5">
+        <Row>
+          <Col className={styles.title}>
+            <h1>Для Офиса</h1>
+            <CardDeck>
+              {offices
+                .slice(indexOfFirstProduct, indexOfLastProduct)
+                .map((product) => (
+                  <Col
+                    sm={12}
+                    md={6}
+                    lg={4}
+                    xl={3}
+                    key={product.id}
+                    className={styles.carddeck}
+                  >
+                    <Card style={{ width: "16rem" }}>
+                      <Card.Img
+                        variant="top"
+                        height="275px"
+                        style={{ objectFit: "cover" }}
+                        src={
+                          product.image
+                            ? product.image.url
+                            : "https://via.placeholder.com/150.png"
+                        }
+                      />
+                      <Card.Body className={styles.cardbody}>
+                        <Card.Title>Подробности в нашем</Card.Title>
+                        <Link href="https://t.me/wooden_mebellari">
+                          <a target="_blank">
+                            <Image
+                              src="/telegram.png"
+                              alt="Telegram logo"
+                              width={50}
+                              height={50}
+                            />
+                          </a>
+                        </Link>
+                        <Link href="https://instagram.com/wooden_jizzakh">
+                          <a target="_blank">
+                            <Image
+                              src="/instagram.png"
+                              alt="Telegram logo"
+                              width={50}
+                              height={50}
+                            />
+                          </a>
+                        </Link>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+            </CardDeck>
+            <div className={styles.pagination}>
+              <Pagination
+                itemClass="page-item"
+                linkClass="page-link"
+                activePage={activePage}
+                itemsCountPerPage={4}
+                totalItemsCount={offices.length}
+                pageRangeDisplayed={5}
+                onChange={handlePageChange}
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <Container className="mb-5">
+        <Row>
+          <Col className={styles.contact}>
+            <Card
+              style={{ width: "20rem", height: "100%" }}
+              className={styles.contact_card}
+            >
+              <Card.Body>
+                <Card.Title>
+                  <i className="fas fa-location-arrow"></i>
+                </Card.Title>
+                <Card.Text>г.Джизак, Напротив гостиницы Джизак</Card.Text>
+              </Card.Body>
+            </Card>
+            <Card
+              style={{ width: "20rem", height: "100%" }}
+              className={styles.contact_card}
+            >
+              <Card.Body>
+                <Card.Title>
+                  <i className="fas fa-phone"></i>
+                </Card.Title>
+                <Card.Text>+998 97 6436566</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <p>Wooden Uz</p>
       </footer>
-    </div>
-  )
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const bedrooms = await axios
+    .get(`${process.env.BACKEND_URL}/products?categories=1`)
+    .then(({ data }) => data)
+    .catch((e) => null);
+
+  const kitchens = await axios
+    .get(`${process.env.BACKEND_URL}/products?categories=2`)
+    .then(({ data }) => data)
+    .catch((e) => null);
+
+  const childrooms = await axios
+    .get(`${process.env.BACKEND_URL}/products?categories=3`)
+    .then(({ data }) => data)
+    .catch((e) => null);
+
+  const offices = await axios
+    .get(`${process.env.BACKEND_URL}/products?categories=4`)
+    .then(({ data }) => data)
+    .catch((e) => null);
+
+  return {
+    props: {
+      bedrooms,
+      kitchens,
+      childrooms,
+      offices,
+    },
+  };
 }
